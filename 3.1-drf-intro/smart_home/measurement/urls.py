e44db1a199efa05359d47a6aaefecfc68cd24043
Stimul88 <stimul881988@mail.ru>
2023-06-19 23:58:38 +0300

    smart_home homework

--- 3.1-drf-intro/smart_home/measurement/urls.py
+++ 3.1-drf-intro/smart_home/measurement/urls.py
@@ -1,5 +1,9 @@
 from django.urls import path
 
+from measurement.views import SensorView, SensorUpdate, AddTemp
+
 urlpatterns = [
-    # TODO: зарегистрируйте необходимые маршруты
+    path('sensors/', SensorView.as_view()),
+    path('sensors/<pk>/', SensorUpdate.as_view()),
+    path('measurements/', AddTemp.as_view()),
 ]
--- 3.1-drf-intro/smart_home/measurement/views.py
+++ 3.1-drf-intro/smart_home/measurement/views.py
@@ -1,2 +1,18 @@
-# TODO: опишите необходимые обработчики, рекомендуется использовать generics APIView классы:
-# TODO: ListCreateAPIView, RetrieveUpdateAPIView, CreateAPIView
+from rest_framework.generics import RetrieveUpdateAPIView, ListCreateAPIView, CreateAPIView
+from measurement.models import Sensor, Measurement
+from .serializers import SensorSerializer, SensorDetailSerializer, MeasurementSerializer
+
+
+class SensorView(ListCreateAPIView):
+    queryset = Sensor.objects.all()
+    serializer_class = SensorSerializer
+
+
+class SensorUpdate(RetrieveUpdateAPIView):
+    queryset = Sensor.objects.all()
+    serializer_class = SensorDetailSerializer
+
+
+class AddTemp(CreateAPIView):
+    queryset = Measurement.objects.all()
+    serializer_class = MeasurementSerializer
